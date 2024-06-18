0f6e601bd472950d09e2c0d1c722d890942760d8
joakim ireri <joeahkim@gmail.com>
2024-06-18 13:10:25 +0300

    deleted:    assets/img/apple-icon.png 	deleted:    assets1/img/apple-touch-icon.png 	new file:   assets1/img/favicon.jpg 	deleted:    assets1/img/favicon.png 	modified:   auth/Untitled.php 	modified:   dashboard.php 	modified:   index.html 	deleted:    rb.png

--- dashboard.php
+++ dashboard.php
@@ -626,7 +626,7 @@ $conn->close();
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/argon-dashboard.min.js?v=2.0.4"></script>
+  <script src="./assets/js/argon-dashboard.min.js?v=2.0.4"></script>
 </body>
 
 </html>
